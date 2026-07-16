232f7d6d23f84f43c5f3a588edea6649f91c8b4f
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-07-16 16:45:26 +0530

    style: improve dashboard layout and scrolling behavior

--- client/src/layouts/DashboardLayout.tsx
+++ client/src/layouts/DashboardLayout.tsx
@@ -5,22 +5,16 @@ import Sidebar from "../components/layout/Sidebar";
 
 const DashboardLayout = () => {
   return (
-    <div className="flex h-screen bg-gray-100">
-
-      {/* Sidebar */}
+    <div className="flex min-h-screen bg-gray-100">
 
       <Sidebar />
 
-      {/* Main Content */}
-
-      <div className="flex flex-1 flex-col overflow-hidden">
+      <div className="flex flex-1 flex-col">
 
         <DashboardNavbar />
 
-        <main className="flex-1 overflow-hidden">
-
+        <main className="flex-1 overflow-y-auto p-6">
           <Outlet />
-
         </main>
 
       </div>
