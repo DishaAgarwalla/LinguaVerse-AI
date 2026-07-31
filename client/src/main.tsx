b82c02e23c134daaaf81b15355d994a59ccff8c7
Disha Agarwalla <dishaagarwalla260@gmail.com>
2026-07-31 21:00:05 +0530

    refactor(app): update application entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
